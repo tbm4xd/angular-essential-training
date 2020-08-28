eb84ae1d02778e7aed734b8151b841392c35ee80
Travis Moe <tbm4xd@virginia.edu>
2020-08-27 21:09:25 -0400

    finished lesson 2.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: '<h1>My App</h1>'
+  selector: 'mw-app',
+  templateUrl: './app.component.html' 
 })
 export class AppComponent {}
